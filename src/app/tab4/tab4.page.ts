e20a2761aa1dd0fa02679711fc89604a64995ca4
Borja <batutxa@fpsanturtzilh.eus>
2024-11-08 20:27:35 +0100

    Second commit

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab4',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  numAdivinar : number=-1;
+  numEscrito : number=0;
+  mensaje : string="Todavía no has introducido ningún numero";
 
-  ngOnInit() {
+  constructor(private alertController: AlertController) { }
+
+  ngOnInit() { 
+    this.numAdivinar=Math.floor(Math.random()*100);
   }
+
+  comprobarNumero (){
+    if (this.numAdivinar == this.numEscrito)
+      this.mensaje="Has acertado";
+    else if (this.numAdivinar > this.numEscrito)
+      this.mensaje ="El número es mayor";
+    else if (this.numAdivinar < this.numEscrito)
+      this.mensaje = "El numero es más pequeño";
+  }
+
+  resetearJuego (){
+    this.mensaje="";
+    this.numEscrito=0;
+    this.numAdivinar=Math.floor(Math.random()*100);
+  }
+
+    async confirmarReinicio() {
+      const alert = await this.alertController.create({
+        header: '¿Nuevo Juego?',
+        buttons: [
+          {
+            text: 'OK',
+            handler: () => {
+              this.resetearJuego(); 
+            },
+          },
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            handler: () => {
+
+            },
+          },
+        ],
+      });
+  
+      await alert.present();
+    }
 
 }
